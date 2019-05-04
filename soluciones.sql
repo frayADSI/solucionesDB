66c1eef556854bb4caf66dde6caedc99f03493bd
frayAdsi <fdpineda81@misena.edu.co>
2019-05-04 08:06:30 -0400

    create TblCliente

--- soluciones.sql
+++ soluciones.sql
@@ -1,2 +1,12 @@
 create database soluciones;
 use soluciones;
+/*next the creation of tables without FK*/
+/*TblCliente creation*/
+create table TblCliente(
+CliNumeroIdentificacion bigint not null,
+CliNombre varchar(20) not null,
+CliApellido varchar(30) not null,
+CliTelefono int not null,
+primary key(CliNumeroIdentificacion) 
+);
+
