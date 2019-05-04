9693aea984d9caff12944d986b437c9bd8f346ea
frayAdsi <fdpineda81@misena.edu.co>
2019-05-04 13:14:25 -0400

    triggers

--- proyecto.sql
+++ proyecto.sql
@@ -104,3 +104,49 @@ RETURN cantidad;
 END
 //
 DELIMITER ;
+
+/*Creation of triggers on client's table*/
+CREATE TABLE Auditoria_Cliente(
+id_Audi int not null auto_increment,
+CliNumeroIdentificacion_Anterior bigint,
+CliNombre_Anterior varchar(20),
+CliApellido_Anterior varchar(30),
+CliTelefono_Anterior int,
+CliNumeroIdentificacion_Nuevo bigint,
+CliNombre_Nuevo varchar(20),
+CliApellido_Nuevo varchar(30),
+CliTelefono_Nuevo int,
+Fecha_Audi datetime,
+usuario varchar(45),
+accion varchar(45) not null,
+primary key(id_Audi)
+);
+
+/*Trigger for update*/
+create trigger auditoria_modificaCliente after update
+on tblcliente
+for each row
+insert  into auditoria_cliente(
+CliNumeroIdentificacion_Anterior,CliNombre_Anterior,CliApellido_Anterior,
+CliTelefono_Anterior,CliNumeroIdentificacion_Nuevo,CliNombre_Nuevo,CliApellido_Nuevo,
+CliTelefono_Nuevo,Fecha_Audi,usuario,accion) values( old.clinumeroidentificacion, old.clinombre, old.cliapellido,
+old.clitelefono,new.clinumeroidentificacion, new.clinombre, new.cliapellido,
+new.clitelefono, now(), current_user(), 'Actualizacion'); 
+
+/*Trigger for delete*/
+create trigger auditoria_eliminaCliente after delete
+on tblcliente
+for each row
+insert  into auditoria_cliente(
+CliNumeroIdentificacion_Anterior,CliNombre_Anterior,CliApellido_Anterior,
+CliTelefono_Anterior,Fecha_Audi,usuario,accion) values( old.clinumeroidentificacion, old.clinombre, old.cliapellido,
+old.clitelefono, now(), current_user(), 'Eliminacion'); 
+
+/*Trigger for insert*/
+create trigger auditoria_insertCliente after insert
+on tblcliente
+for each row
+insert  into auditoria_cliente(
+CliNumeroIdentificacion_Nuevo,CliNombre_Nuevo,CliApellido_Nuevo,
+CliTelefono_Nuevo,Fecha_Audi,usuario,accion) values( new.clinumeroidentificacion, new.clinombre, new.cliapellido,
+new.clitelefono, now(), current_user(), 'insertar'); 
